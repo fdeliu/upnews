b66f6b5fc8998d7432248aedcb792072ecaa5c45
unknown <Flamur Deliu>
2019-12-14 13:52:48 +0100

    Updates

---
+++ precache-manifest.2ae2d09dbbc7a1ce904e5d11474ad7da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bab7a5b38d728c88cc88d363cded083",
+    "url": "/upnews/index.html"
+  },
+  {
+    "revision": "771917c9280164e17bb1",
+    "url": "/upnews/static/css/2.e062bc43.chunk.css"
+  },
+  {
+    "revision": "6ee0a3f7bee60816efbe",
+    "url": "/upnews/static/css/main.e18836e0.chunk.css"
+  },
+  {
+    "revision": "771917c9280164e17bb1",
+    "url": "/upnews/static/js/2.4d486ad4.chunk.js"
+  },
+  {
+    "revision": "fb7e21447a363514a710489c1423fa2e",
+    "url": "/upnews/static/js/2.4d486ad4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ee0a3f7bee60816efbe",
+    "url": "/upnews/static/js/main.a0e0c4bf.chunk.js"
+  },
+  {
+    "revision": "d32c8d74acb9d377bc3d",
+    "url": "/upnews/static/js/runtime-main.5dd8a6fa.js"
+  },
+  {
+    "revision": "bf4c97ed096fec8cdbdbdeeea9a82e72",
+    "url": "/upnews/static/media/loading.bf4c97ed.gif"
+  }
+]);
